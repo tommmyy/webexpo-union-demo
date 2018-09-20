479e864ad389c6b6281ac69e1de531d367cb4d46
Tomáš Konrády <tomaskonrady@gmail.com>
2018-09-20 17:20:31 +0200

    removing code

--- packages/webexpo-app-twentyfifteen/src/routes.js
+++ packages/webexpo-app-twentyfifteen/src/routes.js
@@ -1,18 +1,1 @@
-import loadHeroWidget from 'webexpo-widget-hero';
-import loadNewletterWidget from 'webexpo-widget-newsletter';
-
-export default [
-	{
-		path: 'hero',
-		getComponent: done => {
-			loadHeroWidget(mod => done(mod.default));
-		},
-	},
-	{
-		path: 'newsletter',
-		getComponent: done => {
-			loadNewletterWidget(mod => done(mod.default));
-		},
-	},
-
-];
+export default [];
