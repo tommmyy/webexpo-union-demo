ef2e08d9b990284c8046d395ba43967462823b82
Tomáš Konrády <tomaskonrady@gmail.com>
2018-09-20 17:14:09 +0200

    Adding parrot

--- packages/webexpo-components/src/Banner/Banner.js
+++ packages/webexpo-components/src/Banner/Banner.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Banner as RebassBanner, Heading, Text } from 'rebass';
 import bg from './bg.jpg';
+import parrotBg from './parrot.gif';
 
-const Banner = ({ title, children }) => (
-	<RebassBanner
-		color="white"
-		bg="gray8"
-		backgroundImage={bg}
-	>
-		<Heading>{title}</Heading>
-		<Text
-			textAlign="center"
-			fontWeight="bold"
-		>
-			{children}
-		</Text>
-	</RebassBanner>
-);
+class Banner extends Component {
+	state = { hover: false };
 
+	handleMouseEnter = () => {
+		return this.props.parrot && this.setState({ hover: true });
+	};
+
+	handleMouseLeave = () => {
+		return this.props.parrot && this.setState({ hover: false });
+	};
+	render() {
+		const { title, children, parrot } = this.props;
+		const { hover } = this.state;
+
+		return (
+			<RebassBanner
+				onMouseEnter={this.handleMouseEnter}
+				onMouseLeave={this.handleMouseLeave}
+				bg="gray8"
+				color={parrot && hover ? 'red' : 'white'}
+				backgroundSize={parrot && hover ? 'auto' : 'cover'}
+				backgroundImage={parrot && hover ? parrotBg : bg}
+			>
+				<Heading>{title}</Heading>
+				<Text textAlign="center" fontWeight="bold">
+					{children}
+				</Text>
+			</RebassBanner>
+		);
+	}
+}
 
 Banner.propTypes = {
 	/**
 	 * Content to show in banner area
 	 */
-	children: PropTypes.node.isRequired,
+	children: PropTypes.node,
+	/**
+	 * Enables parrot mode
+	 */
+	parrot: PropTypes.bool,
 	/**
 	 * Banner title message
 	 */
 	title: PropTypes.node,
-
 };
 
 export default Banner;
